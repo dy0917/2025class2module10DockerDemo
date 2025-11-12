68a53a2eaf1d38bd7a93981c7d198b297841546e
dy0917 <huangkingsley@gmail.com>
2025-11-12 20:43:08 +1300

    docker setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+app.get("/", (req, res) => {
+  res.send("Hello Kingsley, we good");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
