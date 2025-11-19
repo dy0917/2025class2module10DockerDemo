5b27ac6b8bc69299c0e037a2e2607f619d36f83a
dy0917 <huangkingsley@gmail.com>
2025-11-19 16:37:46 +1300

    add env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
 const port = 8080;
 app.get("/", (req, res) => {
-  res.send("Hello Kingsley, we good");
+  res.send(`Hello Kingsley, we good ${process.env.CUSTOM_VAR}`);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
